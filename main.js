8a78e78f0ef934a731b0ab5d5391ae879156f60a
Last-Username <61640469+Last-Username@users.noreply.github.com>
2023-03-07 09:53:05 -0500

    Added showhide

--- main.js
+++ main.js
@@ -2,3 +2,15 @@ function startTime() {
         document.getElementById('clock').innerHTML = new Date();
         setTimeout(startTime, 500);
 }
+
+var shown = false;
+function showhide(){
+        if (shown){
+                document.getElementById('email').innerHTML = "show my email";
+                shown = false;
+        }else{
+                var myemail = "<a href='mailto:tiltona1"+ "@" + "udayton.edu'>YOURemailID " +"@" + "udayton.edu</a>";
+                document.getElementById('email').innerHTML= myemail;
+                shown = true;
+        }
+}
